a842e790fa97022b8e59d98412f16fdb637f8ee2
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-20 15:42:52 -0700

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const objectEvent = document.getElementById('input');
+    objectEvent.addEventListener('click', function(){
+        alert('I was Clicked!');
+    })
 }
+
+addingEventListener("input")
