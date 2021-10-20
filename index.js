5aae31b3299af71c350d457427d579765e7c93bf
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-20 15:40:50 -0700

    commit tree 4e392dee4c6a2e7564c3724e3e193e4a0ab9e142

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const objectEvent = document.getElementById('input');
+    console.log(objectEvent);
+    objectEvent.addEventListener('click', function(){
+        alert('I was Clicked!');
+    })
 }
+
+addingEventListener("input")
